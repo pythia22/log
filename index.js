84eba2122d3d88147499e035c56684f135eab3ea
Dim Gar <dgarofalakis@novibet.com>
2022-06-11 18:18:54 +0300

    fix issues with importing

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-var dotenv = require('dotenv');
+import dotenv from 'dotenv';
 dotenv.config();
-var dayjs =  require('dayjs');
+import dayjs from 'dayjs';
 
-exports.LevelEnum = Object.freeze({"DEBUG":"DEBUG", "INFO":"INFO", "ERROR":"ERROR"})
+export const LevelEnum = Object.freeze({"DEBUG":"DEBUG", "INFO":"INFO", "ERROR":"ERROR"})
 
 /**
  * 
  * @param {LevelEnum} level 
  * @param {string} message 
  */
-const log = function () {
+export const log = function () {
     const level = arguments[0];
     const message = Array.prototype.slice.call(arguments, 1);
     // eslint-disable-next-line no-undef
     console.log(`[${dayjs().format(process.env.DATE_FORMAT_CONSOLE)}][${level}]`, message);
 }
-exports.log = log
 /**
  * 
  * @param {LevelEnum} level 
  * @param {string} url 
  * @param {string} method 
  */
-exports.logRequest = function(level, url, method) {
+export const logRequest = function(level, url, method) {
     const message = {
         url: url,
         method: method,
@@ -30,7 +29,7 @@ exports.logRequest = function(level, url, method) {
     log(level, `[REQUEST]`, message)
 }
 
-exports.logResponse = function(level, url, method, statusCode, data) {
+export const logResponse = function(level, url, method, statusCode, data) {
     const message = {
         url: url,
         method: method,
@@ -40,7 +39,7 @@ exports.logResponse = function(level, url, method, statusCode, data) {
     log(level, `[RESPONSE]`, message)
 }
 
-exports.logDB = function(level, query, values) {
+export const logDB = function(level, query, values) {
     const message = {
         query: query,
         values: values,
